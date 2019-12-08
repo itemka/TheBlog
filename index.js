434c28eb45ac1947aae827011f328aeec4534edd
itemka <itemka2503@gmail.com>
2019-12-08 19:54:01 +0300

    Added bodyParser and post request, change get request.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+// parse from data post-require and received all data
+app.use(bodyParser.urlencoded({extendet: true}));
+app.use(bodyParser.json());
 
 const posts = [
     {
@@ -8,43 +13,42 @@ const posts = [
         "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
     },
     {
-        "id": 3,
+        "id": 2,
         "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
         "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
     },
     {
-        "id": 4,
+        "id": 3,
         "title": "eum et est occaecati",
         "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
     },
     {
-        "userId": 1,
-        "id": 5,
+        "id": 4,
         "title": "nesciunt quas odio",
         "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
     },
     {
-        "id": 6,
+        "id": 5,
         "title": "dolorem eum magni eos aperiam quia",
         "body": "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae"
     },
     {
-        "id": 7,
+        "id": 6,
         "title": "magnam facilis autem",
         "body": "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas"
     },
     {
-        "id": 8,
+        "id": 7,
         "title": "dolorem dolore est ipsam",
         "body": "dignissimos aperiam dolorem qui eum\nfacilis quibusdam animi sint suscipit qui sint possimus cum\nquaerat magni maiores excepturi\nipsam ut commodi dolor voluptatum modi aut vitae"
     },
     {
-        "id": 9,
+        "id": 8,
         "title": "nesciunt iure omnis dolorem tempora et accusantium",
         "body": "consectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas"
     },
     {
-        "id": 10,
+        "id": 9,
         "title": "optio molestias id quia eum",
         "body": "quo et expedita modi cum officia vel magni\ndoloribus qui repudiandae\nvero nisi sit\nquos veniam quod sed accusamus veritatis error"
     },
@@ -54,9 +58,18 @@ app.get(`/posts`, (request, response) => {
     return response.send(posts)
 });
 
-app.get(`/posts/:id`, (request, response)=>{
-    const id = request.params.id
-    return response.send(posts[request.params.id]);
+app.get(`/posts/:id`, (request, response) => {
+    const id = request.params.id - 1;
+    return response.send(posts[id]);
 });
 
-app.listen(3000, ()=>console.log('3000 port is listening...'));
+app.post('/posts', (request, response) => {
+    let dataFromUser = request.body;
+    let idNewPost = posts.length + 1;
+    let newData = {...dataFromUser, id: idNewPost};
+    console.log(`Added new post: `, newData);
+    posts.push(newData);
+    return response.send(posts);
+});
+
+app.listen(3000, () => console.log('3000 port is listening...'));
