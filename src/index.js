602418a28d83c750dc6b8af2840d06a8cbe35ac0
itemka <itemka2503@gmail.com>
2019-12-10 00:22:16 +0300

    Added core and change port 3000 -> 333

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 import express from 'express';
 import bodyParser from 'body-parser';
 
+const cors = require('cors');
+
 import PostController from "./controllers/PostController";
 // create instance
 const Post = new PostController();
@@ -14,6 +16,19 @@ mongoose.connect('mongodb://localhost:27017/blogDB', {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+app.use(cors({
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    credentials: true
+}));
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', "http://localhost:3000");
+    res.header('Access-Control-Allow-Headers', true);
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    next();
+});
+
 // get, post, delete, put handlers
 app.get('/posts', Post.index);
 app.post('/posts', Post.create);
@@ -21,4 +36,4 @@ app.get('/posts/:id', Post.read);
 app.delete('/posts/:id', Post.delete);
 app.put('/posts/:id', Post.update);
 
-app.listen(3000, () => console.log('3000 port is listening...'));
+app.listen(333, () => console.log('333 port is listening...'));
